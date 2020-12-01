ca281901038aa315fa290634e4b7072f0bfd6b90
Kshitijkumar05 <Kshitijkumar05@users.noreply.github.com>
2020-12-01 20:46:33 +0530

    style:added chalk
    Used chalk to make the app more colorful and attractive.

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 var readlineSync=require('readline-sync');
+var chalk=require('chalk');
 
-var user=readlineSync.question("What's your name? : ");
-console.log("Welcome "+user +" to the quiz.");
+var user=readlineSync.question(chalk.yellow("What's your name? : "));
+console.log("Welcome "+chalk.blue(user) +" to the quiz.");
 console.log("");
-console.log("Rules: ");
-console.log("1.You will get 2 marks for each correct answer.");
-console.log("2. 1 mark will deduct for wrong answer");
+console.log(chalk.bgYellow("Rules: "));
+console.log(chalk.red("1.You will get 2 marks for each correct answer."));
+console.log(chalk.red("2. 1 mark will deduct for wrong answer"));
 
 console.log(" ");
 
 
 var score=0;
 
-console.log(`LEADERBOARD:
+console.log(chalk.yellowBright(`LEADERBOARD:
 1:Kshitij: 12 points
 2.Kshitij: 10 points
-3:Kshitij: 6 points`);
+3:Kshitij: 6 points`));
 
 function play(question,answer){
   var userAnswer=readlineSync.question(question);
 
   if (userAnswer.toUpperCase() === answer.toUpperCase()){
-    console.log("Right!");
+    console.log(chalk.magentaBright("Right!"));
     score=score+2;
   }else{
-    console.log("Wrong!");
+    console.log(chalk.magentaBright("Wrong!");
     score=score-1;
   }
   console.log("--------");
@@ -32,42 +33,42 @@ function play(question,answer){
  
 var questions = [
   {
-  question : `
+  question :chalk.cyanBright( `
   What is the jersy number of dhoni?
   A: 7
   B: 10
   C: 18
-   `,
+   `),
   answer :"A"},
-{ question :`When did dhoni make ODI debut?
+{ question :chalk.cyanBright(`When did dhoni make ODI debut?
 A: 2003
 B: 2004
 C: 2005
-`,
+`),
 answer: "B"},
-{ question:`Who broke the record of dhoni of scoring highest run in second inning?
+{ question:chalk.cyanBright(`Who broke the record of dhoni of scoring highest run in second inning?
 A:Rohit Sharma
 B:Virat Kohli
 C:Shane watson
-`, 
+`), 
 answer:"C"},
-{question:`When did dhoni get his "A" contract from bcci?
+{question:chalk.cyanBright(`When did dhoni get his "A" contract from bcci?
 A: 2005
 B: 2007
 C: 2011
-`,
+`),
 answer:"b"},
-{ question:`For which IPL team dhoni play?
+{ question:chalk.cyanBright(`For which IPL team dhoni play?
 A: CSK
 B: MI
 C: RCB
-`,
+`),
 answer:"A"},
-{ question:`What is the nick name of dhoni?
+{ question:chalk.cyanBright(`What is the nick name of dhoni?
 A: Cheeku
 B: Mahi
 C: Jumbo
-`,
+`),
 answer:"B"}];
 
 
@@ -76,6 +77,6 @@ for(var i=0; i<questions.length;i++){
   play(ques.question, ques.answer)
   
 }
-console.log("Congratulations!! you scored:" +score +" points");
+console.log(chalk.green("Congratulations!! you scored:" +chalk.underline(score) +" points"));
 console.log("-----------");
-console.log("Thank you for playing the game. If you have scored more than highest score then send me screenshot to get your name featured in leaderboard!");
+console.log(chalk.whiteBright.bgBlue("Thank you for playing the game. If you have scored more than highest score then send me screenshot to get your name featured in leaderboard!"));
